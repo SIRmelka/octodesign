43ce50efd4bba75c43ff81e9b9da7808a2b63328
SIRmelka <melkabotale1@gmail.com>
2022-04-27 13:01:54 +0100

    project initialisation

---
+++ KDA Sessions/Javascript/script.js
@@ -0,0 +1,39 @@
+let message = document.querySelector(".top")
+let content = document.querySelector("#hide")
+let image = document.getElementById("btn");
+let stateimg = false;
+const pass = "Melka";
+let passuse = "";
+image.addEventListener('click', function(){
+
+    stateimg = !stateimg;
+   
+    image.setAttribute('width','100px')
+    if(stateimg==true)
+        {
+            image.setAttribute('src','off.png')
+            content.style.display="none";
+            message.style.display="inline";
+        }
+        else
+        {
+            while(passuse!=pass)
+            {
+                  passuse = prompt("Ente r a password to unlock")
+            }
+            image.setAttribute('src','on.jpg')
+            content.style.display="block";
+            message.style.display="none";
+        }
+            
+
+        }
+)
+
+
+
+
+//localStorage.setItem("killer", "Melkisedeck")
+
+
+d
